598e3ea9b83016cebf2e4071159da2462ea8b874
linardsbi <kaktoss@inbox.lv>
2019-08-23 10:00:46 +0000

    configuration and getting version now works! progress!

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SX1276-python
-rough API for the SX1276 chip that runs on Python
+Rough API for the SX1276 chip that runs on Python
+## Serial error
+*Setting up serial for `Orange PI` on `Armbian`*
+
+1. Find `armbianEnv.txt` in /boot/;
+2. Add `overlays=uart1` to the file and reboot;
+   If adding more than one serial bus, separate names with a comma
+   eg. "overlays=uart1,uart2,uart3" and so on
+3. Create an empty directory:
+   `mkdir /sys/kernel/config/device-tree/overlays/uart1`;
+4. Run `cat /boot/dtb/overlay/sun8i-h3-uart1.dtbo > /sys/kernel/config/device-tree/overlays/uart1/dtbo`;
+5. Run `cat /dev/ttyS1` (depending on the serial port you specified; for uart1 it's ttyS1)
+   Type anything to see if you get a response back.
+
+If it still doesn't work, check [this](https://forum.armbian.com/topic/3557-orangepi-pc-gpio-uart-and-arduino/) or [this](https://forum.armbian.com/topic/1524-orange-pi-one-how-to-enable-uart/) thread on the Armbian forum
--- SX1276.py
+++ SX1276.py
@@ -1,25 +1,9 @@
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 import serial
-import time.sleep
+from time import sleep
 
 gpio.init()
-
-ports = {
-         "M0":  port.PA18,
-         "M1":  port.PA21,
-         "AUX": port.PG8,
-         "UART":  "/dev/ttyS1"}
-
-address = {"HEAD": 0xC0,"ADDH": 0x01,"ADDL": 0x02}
-speed = {"adr": 0b010,"baudrate": 0b011,"parity": 0b00}
-options = {"power": TransmitPower.PWR_17DB, "FEC": 1, "wakeup": 0b000, "drive_mode": 1,"transmission_mode": 1}
-
-# 410~441MHz : 410M + CHAN*1M
-# 855~880.5MHz : 855 + channel * 0.1M
-# eg. channel val (in decimal) = 441 - 410 * 1M || channel val (dec) = (880.5 - 855) * 10M
-
-module = SX1276.begin(ports, address, speed, options)
 
 class ChannelType:
     channel_410 = 0x00
@@ -39,6 +23,12 @@ class ModuleMode:
     POWERSAVE = 2
     SLEEP = 3
 
+
+
+# 410~441MHz : 410M + CHAN*1M
+# 855~880.5MHz : 855 + channel * 0.1M
+# eg. channel val (in decimal) = 441 - 410 * 1M || channel val (dec) = (880.5 - 855) * 10M
+
 required_ports = ["M0", "M1", "AUX", "SERIAL"] 
 default_address = {"HEAD": 0xC0,"ADDH": 0x01,"ADDL": 0x02}
 default_speed = {"adr": 0b010,"baudrate": 0b011,"parity": 0b00}
@@ -46,88 +36,125 @@ default_options = {"power": TransmitPower.PWR_17DB, "FEC": 1, "wakeup": 0b000, "
 
 class SX1276:
     __ports = {}
+    __serial = None
 
-    def begin(ports, address, speed, options):
-        __configPorts(ports)
+    def __init__(self, ports):
+        self.__ports = ports
+    
 
-        # override defaults with specified
-        address = dict(default_address.items() + address.items())
-        speed = dict(default_speed.items() + speed.items())
-        options = dict(default_options.items() + options.items())
+    @classmethod
+    def begin(cls, ports, address, speed, options):
+        module = cls(ports)
+ 
+        module.configPorts()
         
+        # override defaults with specified
+        tmp = {**default_address}
+        tmp.update(address)
+        address = {**tmp}
+
+        tmp = {**default_speed}
+        tmp.update(speed)
+        speed = {**tmp}
+
+        tmp = {**default_options}
+        tmp.update(options)
+        options = {**tmp}
+
         # bitwise magic!
-        SPED = hex(speed["parity"] << 6 | speed["baudrate"] << 3 | speed["adr"])
+        SPED = int(speed["parity"] << 6 | speed["baudrate"] << 3 | speed["adr"])
         CHAN = options["channel"]
-        OPTION = hex(options["transmission_mode"] << 7 | options["drive_mode"] << 6 | options["wakeup"] << 3 | options["FEC"] << 2 | options["power"])
+        OPTION = int(options["transmission_mode"] << 7 | options["drive_mode"] << 6 | options["wakeup"] << 3 | options["FEC"] << 2 | options["power"])
+        
+        parameters = [address["HEAD"], address["ADDH"], address["ADDL"],SPED,CHAN, OPTION]
+        
+        module.changeMode(ModuleMode.SLEEP)
+        module.__writeParameters(parameters)
+        module.changeMode(ModuleMode.NORMAL)
 
-        parameters = [address["HEAD"], address["ADDH"], address["ADDL"],SPED, CHAN, OPTION]
-        __writeParameters(parameters)
+        return module
 
-        return self
-
-    def __configPorts(ports):
+    def configPorts(self):
         for port in required_ports:
-            if port not in ports:
+            if port not in self.__ports:
                 raise ValueError("Missing port: {}".format(port))
 
-        gpio.setcfg(ports["M0"], gpio.OUTPUT)
-        gpio.setcfg(ports["M1"], gpio.OUTPUT)
-        gpio.setcfg(ports["AUX"], gpio.INPUT)
-        serial.Serial(port=ports["SERIAL"], baudrate=9600)
+        gpio.setcfg(self.__ports["M0"], gpio.OUTPUT)
+        gpio.setcfg(self.__ports["M1"], gpio.OUTPUT)
+        gpio.setcfg(self.__ports["AUX"], gpio.INPUT)
         
-        __ports = ports
+        try:
+            self.__serial = serial.Serial(port=self.__ports["SERIAL"], baudrate=9600, timeout=1)
+        except Exception as e:
+            print("Serial error. {} - check README.MD to fix".format(e))
+            exit()
 
-    def sendMessage(message):
+    def sendMessage(self, message):
         pass
     def receiveMessage():
         return ""
-    def changeMode(mode):
+    def changeMode(self, mode):
         if (mode == ModuleMode.NORMAL):
-            gpio.output(__ports["M0"], port.LOW)
-            gpio.output(__ports["M1"], port.LOW)
+            gpio.output(self.__ports["M0"], gpio.LOW)
+            gpio.output(self.__ports["M1"], gpio.LOW)
         elif (mode == ModuleMode.WAKEUP):
-            gpio.output(__ports["M0"], port.HIGH)
-            gpio.output(__ports["M1"], port.LOW)
+            gpio.output(self.__ports["M0"], gpio.HIGH)
+            gpio.output(self.__ports["M1"], gpio.LOW)
         elif (mode == ModuleMode.POWERSAVE):
-            gpio.output(__ports["M0"], port.LOW)
-            gpio.output(__ports["M1"], port.HIGH)
+            gpio.output(self.__ports["M0"], gpio.LOW)
+            gpio.output(self.__ports["M1"], gpio.HIGH)
         elif (mode == ModuleMode.SLEEP):
-            gpio.output(__ports["M0"], port.HIGH)
-            gpio.output(__ports["M1"], port.HIGH)
+            gpio.output(self.__ports["M0"], gpio.HIGH)
+            gpio.output(self.__ports["M1"], gpio.HIGH)
         
         sleep(0.1)            
 
-    def __readBuffer():
+    def __readBuffer(self):
         data = ""
 
         while True:
             line = ""
-            line = serial.readline()
-            
-            if line == "": break
+            line = self.__serial.readline()
 
-            data += line
+            if not line: break
+
+            data += str(line)
         return data
 
 
-    def getParameters():
-         __writeParameters([0xC1,0xC1,0xC1])
+    def getParameters(self):
+        self.changeMode(ModuleMode.SLEEP)
+
+        self.__writeParameters([0xC1,0xC1,0xC1])
         sleep(0.1)
 
-        return __getBuffer()
+        return self.__readBuffer()
 
-    def getVersion():
-        __writeParameters([0xC3,0xC3,0xC3])
+    def getVersion(self):
+        self.changeMode(ModuleMode.SLEEP)
+
+        self.__writeParameters([0xC3,0xC3,0xC3])
         sleep(0.1)
 
-        return __getBuffer()
+        return self.__readBuffer()
     
-    def __resetModule():
-        __writeParameters([0xC4,0xC4,0xC4])
-        
+    def __resetModule(self):
+        self.__writeParameters([0xC4,0xC4,0xC4])
         sleep(1.2)
 
-    def __writeParameters(parameters):
+    def __writeParameters(self,parameters):
         assert(type(parameters) == list)
+        self.__serial.write(bytes(parameters))
 
-        serial.write(parameters)
+ports = {
+         "M0":  port.PA18,
+         "M1":  port.PA21,
+         "AUX": port.PG8,
+         "SERIAL":  "/dev/ttyS1"}
+
+address = {"HEAD": 0xC0,"ADDH": 0x01,"ADDL": 0x02}
+speed = {"adr": 0b010,"baudrate": 0b011,"parity": 0b00}
+options = {"power": TransmitPower.PWR_17DB, "FEC": 1, "wakeup": 0b000, "drive_mode": 1,"transmission_mode": 1}
+
+module = SX1276.begin(ports, address, speed, options)
+print(str(module.getVersion()))
